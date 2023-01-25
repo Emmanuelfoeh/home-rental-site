33f4106449606870c2277fd444f0c879836f0ffe
Emmanuel Foeh <EmmanuelFo@stlghana.com>
2023-01-25 00:02:01 +0000

    done with the raw layout of the page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import PhoneIcon from "@mui/icons-material/Phone";
+import PrintIcon from "@mui/icons-material/Print";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 import useStyles from "./style";
 import { Box } from "@mui/material";
@@ -13,6 +22,8 @@ const Footer = () => {
           width: "85%",
           display: "flex",
           flexDirection: "column",
+          justifyContent: "space-between",
+          padding: "3rem 0",
           // bgcolor: "brown",
           margin: "0 auto",
         }}
@@ -21,34 +32,91 @@ const Footer = () => {
           style={{ width: "100%", height: "2px", backgroundColor: "#F4511E" }}
         ></span>
         <div className={classes.logoSocial}>
-          <div className={classes.footLogo}>LoGo</div>
+          <Box
+            className={classes.footLogo}
+          >
+            <img
+              className={classes.footLogoSelf}
+              style={{ color: "green" }}
+              width="100%"
+              src="/img/logo.svg"
+              alt="logo"
+            />
+          </Box>
           <div className={classes.social}>
-            <div className={classes.socialItem}>
+            <div className={classes.socialItem} style={{ padding: "1rem 0" }}>
+              <FmdGoodIcon
+                style={{ marginRight: "0.5rem", color: "#F4511E" }}
+              />{" "}
               345 Faulconer Drive, Suite 4 • Charlottesville, CA, 12345
             </div>
-            <div className={classes.socialItem}>the two</div>
-            <div className={classes.socialItem}>the troe s sfjg</div>
+            <div
+              className={classes.socialItem}
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <span style={{ display: "flex", alignItems: "center" }}>
+                <PhoneIcon
+                  style={{ marginRight: "0.5rem", color: "#F4511E" }}
+                />{" "}
+                <span>233548505357</span>
+              </span>
+              <span style={{ display: "flex", alignItems: "center" }}>
+                <PrintIcon
+                  className={classes.icon}
+                  style={{ marginRight: "0.5rem", color: "#F4511E" }}
+                />{" "}
+                (123) 456-7890
+              </span>
+            </div>
+            <div
+              className={classes.socialItem}
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <span style={{ marginRight: "1rem", padding: "1rem 0" }}>
+                SocialMedia
+              </span>
+              <span>
+                <FacebookIcon
+                  style={{ marginRight: "1.5rem", color: "#F4511E" }}
+                />
+                <LinkedInIcon
+                  style={{ marginRight: "1.5rem", color: "#F4511E" }}
+                />
+                <TwitterIcon
+                  style={{ marginRight: "1.5rem", color: "#F4511E" }}
+                />
+                <PinterestIcon
+                  style={{ marginRight: "1.5rem", color: "#F4511E" }}
+                />
+                <YouTubeIcon
+                  style={{ marginRight: "1.5rem", color: "#F4511E" }}
+                />
+                <InstagramIcon style={{ color: "#F4511E" }} />
+              </span>
+            </div>
           </div>
         </div>
         <footer className={classes.footerNav}>
           <ul className={classes.footNav}>
             <li>
-              <a href="/">Home</a>
+              <a href="/">About us</a>
             </li>
             <li>
-              <a href="/about">About</a>
+              <a href="/about">contact us</a>
             </li>
             <li>
-              <a href="/con">contact</a>
+              <a href="/con">Help</a>
             </li>
             <li>
-              <a href="/ani">another</a>
+              <a href="/ani">Privacy Policy</a>
             </li>
             <li>
-              <a href="/ano">another</a>
+              <a href="/ano">Disclaimer</a>
             </li>
           </ul>
-          <div className={classes.copyRight}>loerm htlek k lroe js kk</div>
+          <p className={classes.copyRight}>
+            Copyright © 2023 Minimumlivingcost. All rights reserved
+          </p>
         </footer>
       </Box>
     </div>
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -4,15 +4,59 @@ const useStyles = makeStyles()((theme) => {
   return {
     footer: {
       // height: "21rem",
-      border:"1px solid black",
-      padding:"3rem 0",
+      // border: "1px solid black",
+      padding: "3rem 0",
       // backgroundColor: "blue",
     },
-    logoSocial:{
-      display:"flex",
-      columnGap:"2rem",
-    }
+    logoSocial: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: "3rem 4rem",
+      // backgroundColor: "blue",
+      columnGap: "2rem",
+    },
+
+    footerNav: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      borderTop: "2px solid #7BB9FC",
+      width: "100%",
+    },
+    footLogo: {
+      width: "18rem",
+      height: "10rem",
+      padding: "0.5rem",
+      borderRadius: "10px",
+      background:
+        " linear-gradient(32deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)",
+    },
+    footNav: {
+      display: "flex",
+      fontSize: "0.8rem",
+      columnGap: "2rem",
+      "& li": {
+        listStyle: "none",
+        // padding: "0.5rem 0",
+        "& a": {
+          color: "#0A142F",
+          textTransform: "uppercase",
+          textDecoration: "none",
+          "&:hover": {
+            color: "#F4511E",
+            textDecoration: "none",
+          },
+        },
+      },
+    },
+    copyRight: {
+      fontSize: "0.7rem",
+      "&:hover": {
+        color: "#F4511E",
+      },
+    },
   };
 });
 
-export default useStyles
+export default useStyles;
